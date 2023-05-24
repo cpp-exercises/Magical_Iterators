c7ee820ac6372b0b3d5a64075dc01f2b16f1c8fe
neiterman21 <33800420+neiterman21@users.noreply.github.com>
2023-05-24 11:27:45 +0300

    Create Demo.cpp

---
+++ Demo.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "MagicalContainer.h"
+int main() {
+    // Create a MagicalContainer and add some elements
+    MagicalContainer container;
+    container.addElement(17);
+    container.addElement(2);
+    container.addElement(25);
+    container.addElement(9);
+    container.addElement(3);
+
+    // Print container size
+    std::cout << "Size of container: " << container.size() << std::endl;
+
+    // Use AscendingIterator to display elements in ascending order
+    std::cout << "Elements in ascending order:\n";
+    AscendingIterator ascIter(container);
+    for (auto it = ascIter.begin(); it != ascIter.end(); ++it) {
+        std::cout << *it << ' ';
+    }
+    std::cout << std::endl;
+
+    // Use DescendingIterator to display elements in descending order
+    std::cout << "Elements in descending order:\n";
+    DescendingIterator descIter(container);
+    for (auto it = descIter.begin(); it != descIter.end(); ++it) {
+        std::cout << *it << ' ';
+    }
+    std::cout << std::endl;
+
+    // Use PrimeIterator to display prime numbers only
+    std::cout << "Prime numbers:\n";
+    PrimeIterator primeIter(container);
+    for (auto it = primeIter.begin(); it != primeIter.end(); ++it) {
+        std::cout << *it << ' ';
+    }
+    std::cout << std::endl;
+
+    // Remove an element from the container and display the size
+    container.removeElement(9);
+    std::cout << "Size of container after removing an element: " << container.size() << std::endl;
+
+    return 0;
+}
