304b9d15c199c79ec3a69e550cbb343661dec40c
RenanaTurgeman <renanaturgeman058@gmail.com>
2023-05-28 01:07:13 +0300

    basic impliment magicalContainer class

--- sources/MagicalContainer.cpp
+++ sources/MagicalContainer.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "MagicalContainer.hpp"
+using namespace std
+
+MagicalContainer::MagicalContainer(){}
+MagicalContainer::~MagicalContainer(){}
+void MagicalContainer::addElement(int element){
+    elements.push_back(element);
+}
+void MagicalContainer::removeElement(int element){
+    for (auto my_element = elements.begin(); my_element != elements.end(); ++my_element) {
+        if (*my_element == element) {
+            elements.erase(my_element);
+            break;
+        }
+    }
+}
+int MagicalContainer::getSize() const{
+    return elements.size();
+}
--- sources/MagicalContainer.hpp
+++ sources/MagicalContainer.hpp
@@ -4,10 +4,21 @@
 
 #ifndef MAGICAL_ITERATORS_MAGICALCONTAINER_H
 #define MAGICAL_ITERATORS_MAGICALCONTAINER_H
-
-
+#include <iostream>
+#include <vector>
+#include <cmath>
+/*Create a user-defined container class named "MagicalContainer" that can store integers representing mystical elements.
+ * Implement necessary methods for adding elements, removing elements, and retrieving the size of the container.
+ * You can use a dynamic array or any other suitable data structure for internal storage.*/
 class MagicalContainer {
-
+private:
+    std::vector<int> elements;
+public:
+    MagicalContainer();
+    ~MagicalContainer();
+    void addElement(int element);
+    void removeElement(int element);
+    int getSize() const;
 };
 
 
