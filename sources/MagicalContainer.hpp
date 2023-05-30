f8212ffdaab26b5c3bb0e2b5aaf39dd0c067c116
RenanaTurgeman <renanaturgeman058@gmail.com>
2023-05-30 15:27:58 +0300

    start from scratch

--- sources/MagicalContainer.hpp
+++ sources/MagicalContainer.hpp
@@ -1,104 +1,84 @@
-//
-// Created by Renana on 5/25/23.
-//
-
 #ifndef MAGICAL_ITERATORS_MAGICALCONTAINER_H
 #define MAGICAL_ITERATORS_MAGICALCONTAINER_H
 
-#include <iostream>
 #include <vector>
-#include <cmath>
+#include <algorithm>
+#include <stdexcept>
 
 namespace ariel {
-    /*Create a user-defined container class named "MagicalContainer" that can store integers representing mystical elements.
-     * Implement necessary methods for adding elements, removing elements, and retrieving the size of the container.
-     * You can use a dynamic array or any other suitable data structure for internal storage.*/
-
-        class MagicalContainer {
-        private:
-            std::vector<int> elements;
-        public:
-            MagicalContainer();
-            ~MagicalContainer();
-
-            void addElement(int element);
-            void removeElement(int element);
-            int size() const;
-
-            std::vector<int>& getElements();
-            void setElements(std::vector<int> elements);
-
+    class MagicalContainer {
+    private:
+        std::vector<int> elements;
+        bool isPrime(int n);
+    public:
+        void addElement(int elem);
+        void removeElement(int elem);
+        std::vector<int> getElements();
+        int size() const;
 
         class AscendingIterator {
         private:
-            MagicalContainer &container;
+            MagicalContainer* container;
             int index;
 
         public:
-            AscendingIterator(const MagicalContainer &my_container, int idx);
-            AscendingIterator(); //Default constructor
-            AscendingIterator(const AscendingIterator &other); //Copy constructor
-            ~AscendingIterator(); //Destructor
-
-            AscendingIterator &operator=(const AscendingIterator &other); // Assignment operator
-            bool operator==(const AscendingIterator &other) const;
-            bool operator!=(const AscendingIterator &other) const;
-            int operator*() const;
-            AscendingIterator &operator++();
-            bool operator>(const AscendingIterator &other) const;
-            bool operator<(const AscendingIterator &other) const;
-
             AscendingIterator begin() const;
             AscendingIterator end() const;
+            AscendingIterator();
+            AscendingIterator(MagicalContainer* container, int index);
+            AscendingIterator(MagicalContainer* container);
+            AscendingIterator& operator++();
+            bool operator==(const AscendingIterator& it) const;
+            bool operator!=(const AscendingIterator& it) const;
+            bool operator>(const AscendingIterator& it) const;
+            bool operator<(const AscendingIterator& it) const;
+            AscendingIterator& operator<<(int count);
+            AscendingIterator& operator>>(int count);
+            int operator*() const;
         };
 
         class SideCrossIterator {
         private:
-            MagicalContainer &container;
+            MagicalContainer* container;
             int index;
 
         public:
-            SideCrossIterator(const MagicalContainer &cont, int index);
-            SideCrossIterator();
-            SideCrossIterator(const SideCrossIterator &other);
-            ~SideCrossIterator();
-
-            SideCrossIterator &operator=(const SideCrossIterator &other);
-            bool operator==(const SideCrossIterator &other) const;
-            bool operator!=(const SideCrossIterator &other) const;
-            int operator*() const;
-            SideCrossIterator &operator++();
-            bool operator>(const SideCrossIterator &other) const;
-            bool operator<(const SideCrossIterator &other) const;
             SideCrossIterator begin() const;
             SideCrossIterator end() const;
+            SideCrossIterator();
+            SideCrossIterator(MagicalContainer* container, int index);
+            SideCrossIterator(MagicalContainer* container);
+            SideCrossIterator& operator++();
+            bool operator==(const SideCrossIterator& it) const;
+            bool operator!=(const SideCrossIterator& it) const;
+            bool operator>(const SideCrossIterator& it) const;
+            bool operator<(const SideCrossIterator& it) const;
+            SideCrossIterator& operator<<(int count);
+            SideCrossIterator& operator>>(int count);
+            int operator*() const;
         };
 
         class PrimeIterator {
         private:
-            MagicalContainer &container;
-            int currentIndex;
-
-            bool isPrime(int num) const;
+            MagicalContainer* container;
+            int index;
 
         public:
-            PrimeIterator(const MagicalContainer &cont, int idx);
+            PrimeIterator begin() const;
+            PrimeIterator end() const;
             PrimeIterator();
-            PrimeIterator(const PrimeIterator &other);
-            ~PrimeIterator();
-            PrimeIterator &operator=(const PrimeIterator &other);
-            bool operator==(const PrimeIterator &other) const;
-            bool operator!=(const PrimeIterator &other) const;
+            PrimeIterator(MagicalContainer* container, int index);
+            PrimeIterator(MagicalContainer* container);
+            PrimeIterator& operator++();
+            bool operator==(const PrimeIterator& it) const;
+            bool operator!=(const PrimeIterator& it) const;
+            bool operator>(const PrimeIterator& it) const;
+            bool operator<(const PrimeIterator& it) const;
+            PrimeIterator& operator<<(int count);
+            PrimeIterator& operator>>(int count);
             int operator*() const;
-            PrimeIterator &operator++();
-            bool operator>(const PrimeIterator &other) const;
-            bool operator<(const PrimeIterator &other) const;
-            PrimeIterator *begin();
-            PrimeIterator *end();
         };
+    };
+} // namespace ariel
 
-        };//magical
-}//namespace
-
-
-#endif //MAGICAL_ITERATORS_MAGICALCONTAINER_H
+#endif // MAGICAL_ITERATORS_MAGICALCONTAINER_H
